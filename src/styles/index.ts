7c4359855871aa52f717eeff0f393092d6822bed
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-02-18 17:57:37 -0300

    cores

---
+++ src/styles/index.ts
@@ -0,0 +1,15 @@
+import { createStitches } from "@stitches/react";
+
+const { styled, getCssText, globalCss } = createStitches({
+  theme: {
+    color: {
+      primary: `#D73628`,
+      primaryLight: `#FC4737`,
+      primaryDark: `#AD1E12 `,
+      background: `#2F2F33`,
+      title: `#E1E1E6`,
+      text: `#C4C4CC`,
+      shape: `#29292E`,
+    },
+  },
+});
